ae7a2ee60734d7469b310d31f47614782c94c940
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2014-10-10 19:32:12 +0200

    Refactoring code, deps and add kenburns

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 var Q = require("q");
 var Qimage = require("qimage");
-var Qajax = require("qajax");
 var GlslTransition = require("glsl-transition");
-var GlslTransitions = require("glsl-transitions");
 var BezierEasing = require("bezier-easing");
-var Soundcloud = require("./soundcloud");
+var KenBurns = require("kenburns");
 
-function findTransitionByName (name) {
+function defaultFindTransitionByName (name, GlslTransitions) {
   for (var i=0; i<GlslTransitions.length; ++i) {
-    if (GlslTransitions[i].name === name) {
+    if (GlslTransitions[i].name.toLowerCase() === name.toLowerCase()) {
       return GlslTransitions[i];
     }
   }
 }
 
+// Module?
 function extend (obj) {
   var source, prop;
   for (var i = 1, length = arguments.length; i < length; i++) {
@@ -27,97 +26,154 @@ function extend (obj) {
   return obj;
 }
 
-var GLSL_IDENTITY_FROM = "#ifdef GL_ES\nprecision highp float;\n#endif\nuniform sampler2D from, to;\nuniform float progress;\nuniform vec2 resolution;\nvoid main() {vec2 p = gl_FragCoord.xy / resolution.xy;gl_FragColor = texture2D(from, p);}";
+function Viewer (params) {
+  var self = this;
+  if (!params) throw new Error("params are required");
+  ["container", "data"].forEach(function (attr) {
+    if (!(attr in params))
+      throw new Error("params."+attr+" is required.");
+  });
+  
+  // Initial states
+  self.cursor = 0;
+  for (var attr in params) {
+    self[attr] = params[attr];
+  }
+  if (self.GlslTransitions) {
+    self.findTransitionByName = function (name) {
+      return defaultFindTransitionByName(name, self.GlslTransitions);
+    };
+  }
 
-function Viewer (json, canvas, opts) {
-  if (!opts) opts = {};
-  this.data = json;
-  this.cursor = 0;
-  this.canvas = canvas;
-  this.T = GlslTransition(this.canvas);
-  this.identity = this.T(GLSL_IDENTITY_FROM);
-  this.images = {};
-  this.scPlayer = Soundcloud({client_id: opts.soundcloudClientId });
-  json.timeline.forEach(function (item) {
+  self._initDOM();
+
+  var T = GlslTransition(self.canvasT);
+  self.kenBurns = self.canvases.map(function (c) {
+    return new KenBurns.Canvas2D(c); // Currently using Canvas2D because waiting a fix on Chrome
+  });
+  self.curKen = 0;
+
+  self.images = {};
+  self.data.timeline.forEach(function (item) {
     var url = item.image;
-    this.images[url] = Qimage.anonymously(url);
-  }, this);
-  this.transitions = {};
-  json.timeline.forEach(function (item) {
+    self.images[url] = Qimage.anonymously(url);
+  }, self);
+
+  self.transitions = {};
+  self.data.timeline.forEach(function (item) {
     var name = item.transitionNext && item.transitionNext.name;
-    if (name && !(name in this.transitions)) {
-      var transitionObject = findTransitionByName(name);
+    if (name && !(name in self.transitions)) {
+      var transitionObject = self.findTransitionByName(name);
       if (transitionObject) {
-        this.transitions[name] = {
-          t: this.T(transitionObject.glsl),
+        self.transitions[name] = {
+          t: T(transitionObject.glsl),
           uniforms: transitionObject.uniforms
         };
       }
     }
-  }, this);
-  this._preloadAudio(json.music);
-  console.log(json);
+  }, self);
 }
 
 Viewer.prototype = {
+
   start: function () {
     var self = this;
     var data = this.data;
     var item = data.timeline[0];
-    this._startAudio();
+    this._switchCanvas(this.canvases[this.curKen]);
+    return this._prepareNext();
+  },
+
+/////////////////////////////////////////////////////////////////
+
+  _initDOM: function () {
+    var elt = this.container;
+    var dim = elt.getBoundingClientRect();
+    function createCanvas () {
+      var c = document.createElement("canvas");
+      c.width = dim.width;
+      c.height = dim.height;
+      return c;
+    }
+    this.canvasT = createCanvas();
+    this.canvases = [ createCanvas(), createCanvas() ];
+  },
+
+  _switchCanvas: function (c) {
+    var elt = this.container;
+    var child = elt.children[0];
+    if (child) elt.removeChild(child);
+    elt.appendChild(c);
+  },
+
+  _endReached: function () {
+    // TODO event
+  },
+  _performSingleImage: function (item) {
+    var self = this;
+    var cur = self.curKen;
+    self.curKen = cur ? 0 : 1;
     return this.images[item.image]
-      .then(this.displayImage.bind(this))
-      .then(function () {
-        return self.prepareNext();
+      .then(function (img) {
+        var from = KenBurns.crop.largest;
+        var to = KenBurns.crop.largest;
+        if (item.kenburns) {
+          if (item.kenburns.from) from = KenBurns.crop.apply(KenBurns, item.kenburns.from);
+          if (item.kenburns.to) to = KenBurns.crop.apply(KenBurns, item.kenburns.to);
+        }
+        var duration = item.duration - (item.transitionNext && item.transitionNext.duration || 0) / 2;
+        return self.kenBurns[cur].run(img, from, to, duration);
       });
   },
-  end: function () {
-    this._stopAudio();
-  },
-  displayImage: function (img) {
-    return this.identity({ from: img, to: img }, 100);
-  },
-  prepareNext: function () {
+
+  _prepareNext: function () {
+    // Current implementation is a bit crazy and I'm sure we can clarify it.
+
     var self = this;
     var data = this.data;
     var from = this.cursor;
     var endReached = from+1 === data.timeline.length;
-    if (endReached && !data.loop) return this.end();
     var to = endReached ? 0 : from + 1;
     this.cursor = to;
 
-    var itemDuration = data.timeline[from].duration;
-    var transitionNext = data.timeline[from].transitionNext;
-    var transition = transitionNext && transitionNext.name ?
-      this.transitions[transitionNext.name] :
-      { t: this.identity, uniforms: {} };
-    var transitionDuration = transitionNext && transitionNext.duration || 0;
-    var transitionUniforms = transitionNext && transitionNext.uniforms || {};
-    var transitionEasing = BezierEasing.apply(null, transitionNext && transitionNext.easing || [0, 0, 1, 1]);
+    var fromItem = data.timeline[from];
+    var toItem = data.timeline[to];
 
-    return Q.all([
-      this.images[data.timeline[from].image],
-      this.images[data.timeline[to].image]
-    ])
-    .delay(itemDuration)
-    .spread(function (fromImage, toImage) {
-      if (transitionDuration)
-        return transition.t(extend({ from: fromImage, to: toImage }, transition.uniforms, transitionUniforms), transitionDuration, transitionEasing);
-      else
-        return self.displayImage(toImage);
-    })
-    .then(function () {
-      return self.prepareNext();
-    });
-  },
-  _preloadAudio: function (url) {
-    this.scPlayer.load(url);
-  },
-  _startAudio: function () {
-    this.scPlayer.play();
-  },
-  _stopAudio: function () {
-    this.scPlayer.stop();
+    var cur = self.curKen;
+    var oth = cur ? 0 : 1;
+    var fromCanvas = self.canvases[cur];
+    var toCanvas = self.canvases[oth];
+
+    // first, perform the current image.
+    var chain = this._performSingleImage(fromItem);
+
+    // optionally, chain a glsl transition
+    var transitionNext = fromItem.transitionNext;
+    if (transitionNext) {
+      var transition = this.transitions[transitionNext.name];
+      var transitionDuration = transitionNext.duration || 1000;
+      var transitionEasing = BezierEasing.apply(null, transitionNext.easing || [0, 0, 1, 1]);
+      chain = chain
+        .thenResolve(Q.all([
+          this.images[fromItem.image],
+          this.images[toItem.image]
+        ]))
+        .spread(function (fromImage, toImage) {
+          var transitionUniforms = extend({ from: fromCanvas, to: toCanvas }, transition.uniforms || {}, transitionNext.uniforms || {});
+          self._switchCanvas(self.canvasT);
+          return Q.all([
+            transition.t(transitionUniforms, transitionDuration, transitionEasing).then(function () {
+              self._switchCanvas(toCanvas);
+            }),
+            self._prepareNext()
+          ]);
+        });
+    }
+
+    if (endReached && !data.loop)
+      chain = chain.then(this._endReached.bind(this));
+
+    return chain;
   }
 };
 
